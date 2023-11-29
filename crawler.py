beb2370918aea8eb35c5b89666543d537af0c0bd
finchandchips <112412562+finchandchips@users.noreply.github.com>
2023-11-29 07:50:53 -0600

    crawler.py

--- crawler.py
+++ crawler.py
@@ -1,54 +1,48 @@
+import sys
 import requests
 import webbrowser
 import time
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlparse
 
-def get_all_links(url, base_website_url):
-    response = requests.get(url)
-    links = set()
+def get_urls_from_sitemap(sitemap_url):
+    response = requests.get(sitemap_url)
+
+    urls = set()
 
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'lxml')
-        base_url = response.url  # Get the base URL after redirects
+        soup = BeautifulSoup(response.text, 'xml')  # Use 'xml' as the parser
 
-        for a_tag in soup.find_all('a', href=True):
-            link = a_tag['href']
+        # Extract URLs from the sitemap
+        for url_tag in soup.find_all('loc'):
+            url = url_tag.text.strip()
+            urls.add(url)
 
-            # Exclude certain URLs
-            if (
-                link.startswith('javascript:') or
-                link.endswith(('.css', '.php', '.jpg')) or
-                '#' in link
-            ):
-                continue
+    return urls
 
-            # Make the URL absolute using the base URL
-            absolute_url = urljoin(base_url, link)
+def is_valid_url(url):
+    # Check if the URL ends with common image file extensions
+    return not url.endswith(('.jpg', '.jpeg', '.png', '.gif', '.webp'))
 
-            # Check if the absolute URL is from the same domain
-            if urlparse(absolute_url).netloc == urlparse(base_website_url).netloc:
-                links.add(absolute_url)
+def crawl_sitemap(sitemap_url, sitemap_name):
+    # Get URLs from the sitemap
+    sitemap_urls = get_urls_from_sitemap(sitemap_url)
 
-    return links
-
-def crawl_website(url):
     # Set of visited links to avoid duplicates
     visited_links = set()
 
     # Queue for the links to be processed
-    link_queue = [url]
+    link_queue = list(sitemap_urls)
 
     try:
         while link_queue:
             # Get the next link from the queue
             current_link = link_queue.pop(0)
 
-            # Skip if already visited
-            if current_link in visited_links:
+            # Skip if already visited or URL ends with certain file extensions
+            if current_link in visited_links or not is_valid_url(current_link):
                 continue
 
-            print(f"Crawling: {current_link}")
+            print(f"Crawling {sitemap_name}: {current_link}")
 
             # Open the link in the default web browser
             webbrowser.open(current_link, new=0, autoraise=True)  # Open in the same tab
@@ -56,21 +50,30 @@ def crawl_website(url):
             # Add the current link to the set of visited links
             visited_links.add(current_link)
 
-            # Get all links from the current page
-            new_links = get_all_links(current_link, url)
-
-            # Add new links to the queue
-            link_queue.extend(new_links)
-
-            # Introduce a shorter delay between requests (adjust as needed)
-            time.sleep(0.1)  # Reduced delay to 0.5 seconds
+            # Introduce a delay between requests (adjust as needed)
+            time.sleep(1)  # Delay set to 1 seconds
 
     except KeyboardInterrupt:
-        print("Program interrupted by user. Exiting gracefully.")
+        print(f"Program for {sitemap_name} interrupted by user. Exiting gracefully.")
 
 if __name__ == "__main__":
-    # Specify the URL of the website to crawl
-    website_url = "##################"
+    # Check if the main URL is provided as a command line argument
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <main_url>")
+        sys.exit(1)
 
-    # Call the crawl_website function
-    crawl_website(website_url)
+    main_url = sys.argv[1]
+
+    # Specify the URLs of the sitemaps
+    sitemap_url_1 = f"{main_url}/page-sitemap.xml"
+    sitemap_url_2 = f"{main_url}/product-sitemap.xml"
+    sitemap_url_3 = f"{main_url}/post-sitemap.xml"
+
+    # Call the crawl_sitemap function for the first sitemap
+    crawl_sitemap(sitemap_url_1, "Sitemap 1")
+
+    # Call the crawl_sitemap function for the second sitemap
+    crawl_sitemap(sitemap_url_2, "Product Sitemap")
+
+    # Call the crawl_sitemap function for the third sitemap
+    crawl_sitemap(sitemap_url_3, "Post Sitemap")
